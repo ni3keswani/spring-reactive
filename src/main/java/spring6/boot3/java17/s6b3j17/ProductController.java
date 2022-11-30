a8cf7dd7c39b1539ab91c750e057fc70d8f17344
nitin <nitin.keswani289@gmail.com>
2022-11-30 12:51:17 +0530

    commenting NPE

--- src/main/java/spring6/boot3/java17/s6b3j17/ProductController.java
+++ src/main/java/spring6/boot3/java17/s6b3j17/ProductController.java
@@ -2,12 +2,9 @@ package spring6.boot3.java17.s6b3j17;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.context.ReactiveSecurityContextHolder;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -20,12 +17,12 @@ public class ProductController {
     @GetMapping(value = "/products", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     Flux<Product> getAll() {
         Flux<Product> all = productService.getAll();
-        ReactiveSecurityContextHolder
+        /*ReactiveSecurityContextHolder
                 .getContext()
                 .map(e -> e.getAuthentication())
                 .log()
                 .doOnSuccess(e -> System.out.print(e.toString()))
-                .subscribe();
+                .subscribe();*/
         return all;
     }
 
